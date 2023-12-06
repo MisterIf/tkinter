4cde49dceea779c89e1f70bbc23bdd32cc5f61a9
Pavlo Krupitsa <pkrupitsa@gmail.com>
2023-12-06 21:10:56 +0100

    Add button but1

---
+++ proba_loc.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+window = Tk()
+window.title('Git work')
+window.geometry('360x240')
+
+but1 = Button(window, text='Open', font='Arial 12')
+but1.place(x=150, y=100)
+
+window.mainloop()
