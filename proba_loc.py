665cd960a1b3aedbee2b68235353f4d96fd41b2d
Pavlo Krupitsa <MisterIf@users.noreply.github.com>
2023-12-10 21:15:30 +0100

    Add lab_info

--- proba_loc.py
+++ proba_loc.py
@@ -3,6 +3,7 @@ from tkinter import *
 def set_color():
     but1.config(bg='green')
     lab.config(bg='green')
+    lab_info.config(text='Color changed')
 
 window = Tk()
 window.title('Git work')
@@ -12,5 +13,7 @@ but1 = Button(window, text='Open', font='Arial 12', command=set_color)
 but1.place(x=150, y=100)
 lab = Label(window, text='New widget')
 lab.place(x=10, y=10)
+lab_info = Label(window, text='Without changes')
+lab_info.place(x=10, y=150)
 
 window.mainloop()
