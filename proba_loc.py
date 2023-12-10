ce362cc8b96e66139aa71c98e95ab8745a6eedd4
Pavlo Krupitsa <pkrupitsa@gmail.com>
2023-12-10 19:42:34 +0100

    Add def for but1

--- proba_loc.py
+++ proba_loc.py
@@ -1,11 +1,13 @@
 from tkinter import *
 
+def set_color():
+    but1.config(bg='green')
 
 window = Tk()
 window.title('Git work')
 window.geometry('360x240')
 
-but1 = Button(window, text='Open', font='Arial 12')
+but1 = Button(window, text='Open', font='Arial 12', command=set_color)
 but1.place(x=150, y=100)
 
 window.mainloop()
